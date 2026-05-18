480c0d915bbf165a0498248779b7370b7d4b8948
윤석모 <seokmoyoon@gmail.com>
2026-05-18 15:39:44 +0900

    메인수정

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const baseURL = import.meta.env.BASE_URL; // GitHub Pages 경로 설정
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+console.log(`Base URL: ${baseURL}`); // 디버깅용 출력
