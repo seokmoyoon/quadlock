1cde77ac329f6b1c09e77cb5eb39527fea9ad9b8
윤석모 <seokmoyoon@gmail.com>
2026-05-18 15:37:09 +0900

    열기위한수정

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/quadlock/', // GitHub Pages 배포를 위한 base 경로 설정
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
